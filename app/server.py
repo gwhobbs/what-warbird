9c717bc8a5e0ce5fd4ea82ff8559255e3ce779f8
Grant Hobbs <gwhobbs@users.noreply.github.com>
2019-08-25 22:40:00 -0500

    Add warbirds

--- app/server.py
+++ app/server.py
@@ -9,10 +9,10 @@ from starlette.middleware.cors import CORSMiddleware
 from starlette.responses import HTMLResponse, JSONResponse
 from starlette.staticfiles import StaticFiles
 
-export_file_url = 'https://www.dropbox.com/s/6bgq8t6yextloqp/export.pkl?raw=1'
-export_file_name = 'export.pkl'
+export_file_url = 'https://www.dropbox.com/s/iiedoskamb0ci3c/warbirds-v1.pkl?dl=1'
+export_file_name = 'warbirds-v1.pkl'
 
-classes = ['black', 'grizzly', 'teddys']
+classes = ['p51', 'p47', 'p38', 'fw190', 'me109', 'spitfire']
 path = Path(__file__).parent
 
 app = Starlette()
